184d8476bb01bcd2bfef6953a89c10073f7764b8
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-06 14:07:57 +0000

    Counts to users number

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Counts to users number
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner
+    Scanner input = new Scanner(System.in);
+    //ask user for integer
+    System.out.println("Please enter a positive integer");
+    //declare a variable to store integer
+    int integer = input.nextInt();
+    //create a loop that counts up to users number
+    for(int count = 1; count <= integer; count++ ){
+     if(count == integer){
+       System.out.print(integer);
+     }else if(count < integer){ 
+       System.out.print(count + ",");
+     }
+    } 
     
   }
 }
